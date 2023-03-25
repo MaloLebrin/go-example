1a67b91662b9f8f0e29b1a4ce83dc571392da74f
malolebrin <malolebrin@gmail.com>
2023-03-25 17:09:10 +0100

    update Go app

--- app.go
+++ app.go
@@ -28,6 +28,6 @@ func main() {
 		t.ExecuteTemplate(w, "index.html.tmpl", data)
 	})
 
-	log.Println("Test go exemple listening on", port)
+	log.Println("Test go exemple testr listening on", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
